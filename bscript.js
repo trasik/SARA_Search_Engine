e0b7c3e098aa3c6c356e1188ab709d125b926353
Torendra Rasik <torendrarasik@gmail.com>
2019-02-25 10:37:38 -0500

    files added.

---
+++ bscript.js
@@ -0,0 +1,49 @@
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var h = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+var windowscr = document.getElementById("winfo");
+windowscr.innerHTML = "Browser inner window width: " + w + ", height: " + h + ".";
+
+var scr = document.getElementById("sinfo");
+scr.innerHTML = "Screen Width: " + screen.width + "<br />"
+                + "Screen Height: " + screen.height + "<br />"
+                + "Screen Height: " + screen.height + "<br />"
+                + "Available Screen Width: " + screen.availWidth + "<br />"
+                + "Available Screen Height: " + screen.availHeight  + "<br />"
+                + "Screen Color Depth: " + screen.colorDepth + "<br />"
+                + "Screen Pixel Depth: " + screen.pixelDepth +"<br />";
+
+var browser = document.getElementById("brinfo");
+browser.innerHTML = "Are cookies enabled? " + navigator.cookieEnabled + "<br />"
+                    + "Is Java enabled? " + navigator.javaEnabled() + "<br />"
+                    + "Browser Name: " + navigator.appName + "<br />"
+                    + "Browser Codename: " + navigator.appCodeName + "<br />"
+                    + "Browser Engine: " + navigator.product + "<br />"
+                    + "Browser Platform: " + navigator.platform + "<br />"
+                    + "Browser Engine: " + navigator.language + "<br />";
+
+var loc = document.getElementById("linfo");
+loc.innerHTML = "Page Location: " + window.location.href + "<br />"
+                    + "Page Hostname: " + window.location.hostname + "<br />"
+                    + "Page Path: " + window.location.pathname + "<br />"
+                    + "Page Protocol " + window.location.protocol + "<br />"
+                    + "Port Number: " + window.location.port + "<br />";
+
+var x = document.getElementById("geoinfo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+                "<br>Longitude: " + position.coords.longitude;
+}
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction(id) {
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-  var openDropdown = dropdowns[i];
-    openDropdown.classList.remove('show');
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  document.getElementById(id).classList.toggle("show");
 }
 
 function validateForm() {
@@ -15,18 +12,5 @@ function validateForm() {
   if (x != "Pizza" && x != "pizza") {
     alert("Only search that works right now is for 'Pizza'");
     return false;
-  }
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
-      }
-    }
   }
 }
